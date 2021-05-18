a58bcad46e6abcd771af70cb3689a445aa863140
kaiwolff <66526750+kaiwolff@users.noreply.github.com>
2021-05-18 12:00:10 +0100

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,37 @@
+#Arithmetic Operators
+# + - / *
+#Comparison Operators
+#>
+# <
+# ==
+
+value1 = 6
+value2 = 7
+
+# print(value2 > value1) #returns true if value2 is bigger than value1
+# print(value1 > value2) #as above but values reversed
+# print(value1 + value2)
+# print(value1 - value2)
+# print(value1 >= value2)
+# print(value1 != value2)
+
+
+#Built in methods available that give us boolean outcome
+#e.g.
+
+name = "James"
+#isalpha checks if all the character are letters
+print(name.isalpha())
+
+#if this was a number, would need to use casting:
+
+# age = str(2)
+# print(age.isalpha())
+print(name[0])
+print(name.startswith(name[0].upper()))
+
+#Now: Activity - add this to documentation with own notes.
+#Record video of yourself:
+    #What is the role of DevSecOps
+    #Why Python and how does it fit into DevSecOps
+    #Why DevSecOps?
